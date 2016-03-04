993f1482affa4663a19f95cf7e1cd5eaee20b5ae
Coranna Howard <me@komiga.com>
2016-03-04 15:51:25 -0500

    Dialect: added attachment class interface; register_dialect(): register attachments.

--- core/sys/Dialect/Dialect.lua
+++ core/sys/Dialect/Dialect.lua
@@ -7,13 +7,22 @@ local Tracker = require "Quanta.Tracker"
 local M = U.module(...)
 
 M.actions = {}
+M.attachments = {}
 
 function M.add_action(name, class)
 	U.assert(not M.actions[name])
 	M.actions[name] = class
 end
 
+function M.add_attachment(name, class)
+	U.assert(not M.attachments[name])
+	M.attachments[name] = class
+end
+
 function M.register_dialect(director)
+	for name, attachment in pairs(M.attachments) do
+		director:register_attachment(name, attachment)
+	end
 	for name, action in pairs(M.actions) do
 		director:register_action(name, action)
 	end
@@ -71,6 +80,51 @@ function M.make_action(mod, name, setup)
 	class.t_head:build()
 
 	Dialect.add_action(class.name, class)
+
+	return class
+end
+
+function M.make_attachment(mod, name, setup)
+	U.type_assert(mod, "table")
+	U.type_assert(name, "string")
+	U.type_assert(setup, "function")
+
+	local class = U.class(mod[name])
+	mod[name] = class
+	class.name = name
+
+	function class:__init()
+	end
+
+	function class:from_object(context, tracker, attachment, obj)
+		return context:consume(class.t_head, obj, self)
+	end
+
+	function class:to_object(action, obj)
+	end
+
+	class.t_body = Match.Tree()
+	class.t_head_tags = Match.Tree()
+	class.t_head = Match.Tree({
+	Match.Pattern{
+		name = Match.Any,
+		vtype = Match.Any,
+		tags = class.t_head_tags,
+		children = class.t_body,
+		acceptor = function(context, self, obj)
+			return class.acceptor(context, self, obj)
+		end,
+	},
+	})
+
+	setup(class)
+	U.type_assert(class.acceptor, "function")
+
+	class.t_body:build()
+	class.t_head_tags:build()
+	class.t_head:build()
+
+	Dialect.add_attachment(class.name, class)
 
 	return class
 end
